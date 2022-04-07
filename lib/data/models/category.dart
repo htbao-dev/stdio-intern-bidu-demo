39a3bfed6151801fa47c5c13b367d15f1a9c693c
htbao-dev <ht.bao1112000@gmail.com>
2022-04-07 18:00:50 +0700

    add assets

---
+++ lib/data/models/category.dart
@@ -0,0 +1,45 @@
+// To parse this JSON data, do
+//
+//     final category = categoryFromMap(jsonString);
+
+import 'dart:convert';
+
+class Category {
+  Category({
+    required this.id,
+    required this.name,
+    // @required this.nameLocalized,
+    required this.shopId,
+    // @required this.permalink,
+    required this.parentId,
+    required this.priority,
+    required this.childs,
+    required this.isActive,
+    required this.avatar,
+    required this.pdfAvatar,
+  });
+
+  String id;
+  String name;
+  String shopId;
+  String parentId;
+  int? priority;
+  dynamic childs;
+  bool isActive;
+  String avatar;
+  String pdfAvatar;
+
+  factory Category.fromJson(String str) => Category.fromMap(json.decode(str));
+
+  factory Category.fromMap(Map<String, dynamic> json) => Category(
+        id: json["_id"],
+        name: json["name"],
+        shopId: json["shop_id"],
+        parentId: json["parent_id"],
+        priority: json["priority"],
+        childs: json["childs"],
+        isActive: json["is_active"],
+        avatar: json["avatar"],
+        pdfAvatar: json["pdfAvatar"],
+      );
+}
