68ae16be12b4016201d1370e72ff2bd4b608c04a
htbao-dev <ht.bao1112000@gmail.com>
2022-04-08 23:34:14 +0700

    hide android status bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:bidu_demo/data/repositories/home_repository.dart';
 import 'package:bidu_demo/logic/blocs/home_bloc/home_bloc.dart';
 import 'package:bidu_demo/presentation/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const BiduApp());
 }
 
@@ -25,7 +29,8 @@ class BiduApp extends StatelessWidget {
       ),
       theme: ThemeData(
           primaryColor: Colors.white,
-          colorScheme: ColorScheme.fromSwatch(accentColor: Colors.grey)),
+          colorScheme: ColorScheme.fromSwatch(accentColor: Colors.grey),
+          fontFamily: 'Lexend'),
     );
   }
 }
